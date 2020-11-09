d7b512e870e06e43a1a538d59041fcda780c2811
Pedro Zunino <you@example.com>
2020-11-08 21:12:01 -0300

    Last

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
-
+ const { v4: uuid, validate: isUuid } = require('uuid');
 const app = express();
 
 app.use(express.json());
@@ -11,23 +10,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  }
+  repositories.push(repository);
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params; 
+  const {title,url,techs,likes} = request.body
+  const repositoryId = repositories.findIndex(repository => repository.id == id)
+  
+
+  if(repositoryId>=0){
+    const repository = {
+      id,title,url,techs,likes: 1,
+    }
+    repositories[repositoryId] = repository
+    return response.status(200).json(repository)}
+  else{
+    return response.status(400).json({error:"Error"})
+  }
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params; 
+  const repositoryId = repositories.findIndex(repository => repository.id == id)
+  if(repositoryId>=0){
+    repositories.splice(repositoryId,1)
+    return response.status(204).send()
+  }
+  else{
+    return response.status(400).json({error:"Error"})
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params; 
+  const {title,url,techs,likes} = request.body
+  const repositoryId = repositories.findIndex(repository => repository.id == id);
+  if(repositoryId>=0){
+    repositories[repositoryId].likes +=1
+    return response.json(repositories[repositoryId])
+  }
+  else{
+    return response.status(400).json({error:"Error"})
+  }
 });
 
 module.exports = app;
